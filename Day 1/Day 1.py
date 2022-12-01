8776744d425e7c29a66035043baa64c128efbcb3
Nicola Dario Losito <nicoladario.losito@gmail.com>
2022-12-01 18:39:41 +0100

    Day 1 Part 2

--- Day 1/Day 1.py
+++ Day 1/Day 1.py
@@ -15,16 +15,16 @@ INPUT = "input.txt"
 with open(PATH+INPUT) as file:
     calories = []
     elf = []
-    for text_line in file:
+    for text_line in file:                              #calories is a list of each elf inventory
         if text_line == "\n":
             calories.append(elf)
             elf = []
         else: elf.append(int(text_line.strip()))
-        
-    most = 0
-    for item in calories:
-        cal = sum(item)
-        if cal > most: most = cal
+    calories.append(elf)                                #accounting for the last line in the file
     
-    print("Part 1: "+str(most))
+    total = [sum(item) for item in calories]            #total is a list of each elf total calories carried
+    total = sorted(total, reverse=True)
+    
+    print("Part 1: "+str(total[0]))                     #Part 1 asks the top calories carried
+    print("Part 2: "+str(total[0]+total[1]+total[2]))   #Part 2 asks the sum of the top three
     
