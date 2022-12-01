a3ad13e49f5dd50568bf5303195610462797cdd6
Nicola Dario Losito <nicoladario.losito@gmail.com>
2022-12-01 17:57:42 +0100

    First Commit

---
+++ Day 1/Day 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 17:43:31 2022
+
+Advent of Code 2022
+Day 1
+
+@author: NaimarDL
+"""
+
+PATH = 'C:/Users/Utente/Desktop/Advent 2021/Day 15/'
+TEST = "test.txt"
+INPUT = "input.txt"
+
---
+++ README.txt
@@ -0,0 +1,2 @@
+My solutions to Advent of Code 2022
+Also a challenging and engaging way for me to learn Python
