0f2a74a959592d218d4788fbcc2fb56e7c3b53c8
Nicola Dario Losito <nicoladario.losito@gmail.com>
2022-12-05 17:50:52 +0100

    Day 4 part 1 and 2

--- Day 4/Day 4.py
+++ Day 4/Day 4.py
@@ -11,3 +11,30 @@ Day 4
 EXAMPLE = "example.txt"
 INPUT = "input.txt"
 
+def contain(item):
+    if item[0][0] >= item[1][0] and item[0][1] <= item[1][1]: return True
+    elif item[1][0] >= item[0][0] and item[1][1] <= item[0][1]: return True
+    return False
+
+def overlap(item):
+    if item[0][0] <= item[1][0] and item[0][1] >= item[1][0]: return True
+    elif item[0][0] <= item[1][1] and item[0][1] >= item[1][1]: return True
+    elif item[1][0] >= item[0][0] and item[1][1] <= item[0][0]: return True
+    elif item[1][0] >= item[0][1] and item[1][1] <= item[0][1]: return True
+    return False
+
+with open(INPUT) as file:
+    text = file.read().split("\n")
+    text = [list(list(int(item) for item in tup.split("-")) for tup in tup.split(",")) for tup in text]
+    
+    contains = 0
+    overlaps = 0
+    for item in text:
+        if contain(item): 
+            contains+=1
+            overlaps+=1
+        elif overlap(item):
+            overlaps+=1
+    
+    print(contains)
+    print(overlaps)
